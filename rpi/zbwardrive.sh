3e3945572a56d8d476040a38a3bf119f2d85400e
Oliver Jensen <oliver.jensen@praetorian.com>
2016-08-08 11:18:32 -0500

    more cleanup

--- rpi/zbwardrive.sh
+++ rpi/zbwardrive.sh
@@ -1,15 +1,3 @@
 PCAP_DIR=/tmp/pcaps
-RUNDIR=/run/gpsd
-NAME=gpsd
-DAEMON=/usr/sbin/$NAME
-GPSD_SOCKET=/var/run/gpsd.sock
-GPSD_OPTIONS="$DEVICE -F $GPSD_SOCKET"
-
-# mkdir -p $PCAP_DIR
-mkdir -p $RUNDIR
-
-service gpsd stop
-# Run gpsd with correct params
-$DAEMON $GPSD_OPTIONS > /tmp/gpsdout
 
 (cd $PCAP_DIR && zbwardrive -s 2 -c 7 -v -g > /tmp/zbout)
